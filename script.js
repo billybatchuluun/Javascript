740778dad6533429888bb1b19b4163417cb75072
Bilguun Batchuluun <23LP6327@G304-CC0073.local>
2023-10-30 18:03:38 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// let num=100;
+// while(num>0){console.log(num);
+// num--}
+
+// let num=0;
+// do{num++;
+// console.log(num);} while (num<50);
+
+let passcode;
+
+do{ passcode=prompt("please enter you password");
+} while (passcode!=="sesame");
+alert("Access granted") ;
+
+
+
