b6a4137da389523551d52c3e1b1e20f4e80316e5
Bilguun Batchuluun <23LP6327@G304-CC0073.local>
2023-10-30 19:05:41 +0800

    update from class

--- script.js
+++ script.js
@@ -6,11 +6,23 @@
 // do{num++;
 // console.log(num);} while (num<50);
 
-let passcode;
+// let passcode;
+// do{ passcode=prompt("please enter you password");
+// } while (passcode!=="sesame");
+// alert("Access granted") ;
 
-do{ passcode=prompt("please enter you password");
-} while (passcode!=="sesame");
-alert("Access granted") ;
 
+
+
+// for(let num=0; num<10; num++){alert.log(num)}
+
+// let sum=0;
+// let num=prompt("enter any number")
+// for(i=1; i<=num; i++){if(i%2==0){sum=sum+i}} alert(sum)
+
+let sum=0;
+let n=prompt("enter any number");
+for(i=1; i<=n; i++; sum=sum+i)
+alert(sum)
 
 
