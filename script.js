293fff0a806570b3388d3137a16ce370bbaf0f84
Bilguun Batchuluun <23LP6327@G304-CC0073.local>
2023-10-31 18:51:20 +0800

    update

--- script.js
+++ script.js
@@ -11,18 +11,61 @@
 // } while (passcode!=="sesame");
 // alert("Access granted") ;
 
-
-
-
 // for(let num=0; num<10; num++){alert.log(num)}
 
 // let sum=0;
 // let num=prompt("enter any number")
 // for(i=1; i<=num; i++){if(i%2==0){sum=sum+i}} alert(sum)
 
-let sum=0;
-let n=prompt("enter any number");
-for(i=1; i<=n; i++; sum=sum+i)
-alert(sum)
+// let sum=0;
+// let n=prompt("enter any number");
+// for(let i=0; i<=n; i++) {sum+=i};
+// alert(sum)
 
+//ugugdsun toonii heden zereg 100 ih ve?
 
+// let n=prompt("enter any number");
+
+//1-1000 хүртэлх тэгш тоонуудын нийлбэрийг ол.
+// let sum=0;
+// for(i=2; i<=1000; i++){if(i%2==0){sum=sum+i}} {console.log(sum)}
+
+// let a=prompt("enter any number")
+// let b=prompt("enter any number")
+// if (a>b)
+
+// let a = prompt("please enter number")
+// let result="";
+// // for(i=1; i<=a; i++){result+="*"; console.log(result)}
+// for(let i = 1; i <= a; i++) {
+//     let str = "";
+//     if(i % 2 === 0) {
+//         console.log("tegsh too shuu");
+//     } else {
+//         console.log("sondgoi too shuu");
+//     }
+
+// }
+
+// do {
+//     console.log("object");
+// }
+
+// let a=prompt("please enter number")
+// let result="";
+// for(let i=1; i<=a; i--){result+="*"}
+
+let a = 5;
+
+for (let i = 5; i > 0; i--) {
+  // 4
+  let row = "";
+  for (let j = i; j > 0; j--) {
+    // 0
+
+    row += "* "; // "* * * * * "
+  }
+  console.log(row);
+}
+// * * * * *
+// * * * *
