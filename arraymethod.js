3c2468d7ad279885d629c8bc636f08fda01b796a
Bilguun Batchuluun <23LP6327@G304-CC0073.local>
2023-11-10 19:29:50 +0800

    update

---
+++ arraymethod.js
@@ -0,0 +1,27 @@
+//Өгөгдсөн object - ийг өсөх дарааллаар эрэмбэлcэн array буцаадаг функц бич.
+
+// let num = { b: 2, a: 1, c: 3, d: 4 };
+
+// console.log(Object.entries(num).sort());
+
+// let num = { b: 2, a: 1, c: 3, d: 4 };
+
+// console.log(Object.keys(num).sort());
+
+// Овог нэр 2-ыг нийлүүлж бүтэн нэрийг харуулах.
+// Овогийг том үсгээр харуулах.
+// Овог эсвэл Нэр аль нэг нь байхгүй бол "Буруу утга оруулсан байна." буцаах.
+
+let user = {
+  firstName: "Micheal",
+  lastName: "Jordan",
+};
+let toUpperCase = user.firstName.toUpperCase();
+
+console.log(toUpperCase + lastName);
+
+// let user = {
+//   name: "John",
+//   age: 30,
+// };
+// console.log(Object.values(user));
